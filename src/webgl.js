3cf00b88aee26bc94775f3d84976f7599087411c
Tim Martin <starwed@gmail.com>
2014-01-08 20:16:53 -0500

    Buncha updates, TestColor component

--- src/webgl.js
+++ src/webgl.js
@@ -6,7 +6,14 @@ var FRAGMENT_SHADER_SRC =
   "precision mediump float;"
 
   +"void main(void) {"
-  +"gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);"
+  +"gl_FragColor = vec4(0.0, 1.0, 1.0, 0.5);"
+  +"}";
+
+var FRAGMENT_SHADER_SRC_2 = 
+  "precision mediump float;"
+
+  +"void main(void) {"
+  +"gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);"
   +"}";
 
 // test vertex shader
@@ -17,21 +24,119 @@ var VERTEX_SHADER_SRC_OLD =
   + " gl_Position = vec4(aVertexPosition, 0, 1);"
   + "}";
 
-var VERTEX_SHADER_SRC = 
-"attribute vec2 a_position;\r\nuniform float uViewportWidth;\r\nuniform float uViewportHeight;\r\nuniform float uViewportX;\r\nuniform float uViewportY;\r\n\r\nuniform float uEntityWidth;\r\nuniform float uEntityHeight;\r\nuniform float uEntityX;\r\nuniform float uEntityY;\r\n\r\nuniform float uOriginX;\r\nuniform float uOriginY;\r\nuniform float uEntityRotation;\r\n\r\n\r\nmat4 viewportScale = mat4(2.0 \/ uViewportWidth,0,0,0, 0,-2.0 \/ uViewportHeight, 0,0, 0,0,1,0, -1, +1.0,0,1);\r\nvec4 viewportTranslation = vec4(uViewportX, uViewportY, 0, 0);\r\n\r\nmat2 entityScale = mat2( uEntityWidth, 0, 0, uEntityHeight);\r\nvec2 entityTranslation = vec2(uEntityX, uEntityY);\r\nvec2 entityOrigin = vec2(uOriginX, uOriginY);\r\nmat2 entityRotationMatrix = mat2(cos(uEntityRotation), sin(uEntityRotation), -sin(uEntityRotation), cos(uEntityRotation));\r\n\r\nvoid main() {\r\n  vec2 pos = entityScale * a_position;\r\n  pos = entityRotationMatrix * (pos - entityOrigin) + entityOrigin + entityTranslation;\r\n  gl_Position = viewportScale * (viewportTranslation + vec4(pos, 0, 1) );\r\n}"
+// Vertex shader source, unformatted
+/*
+attribute vec2 a_position;
+uniform float uViewportWidth;
+uniform float uViewportHeight;
+uniform float uViewportX;
+uniform float uViewportY;
 
+uniform float uEntityWidth;
+uniform float uEntityHeight;
+uniform float uEntityX;
+uniform float uEntityY;
+uniform float uEntityZ;
+
+uniform float uOriginX;
+uniform float uOriginY;
+uniform float uEntityRotation;
+
+
+mat4 viewportScale = mat4(2.0 / uViewportWidth,0,0,0, 0,-2.0 / uViewportHeight, 0,0, 0,0,1,0, -1, +1.0,0,1);
+vec4 viewportTranslation = vec4(uViewportX, uViewportY, 0, 0);
+
+mat2 entityScale = mat2( uEntityWidth, 0, 0, uEntityHeight);
+vec2 entityTranslation = vec2(uEntityX, uEntityY);
+vec2 entityOrigin = vec2(uOriginX, uOriginY);
+mat2 entityRotationMatrix = mat2(cos(uEntityRotation), sin(uEntityRotation), -sin(uEntityRotation), cos(uEntityRotation));
+
+void main() {
+  vec2 pos = entityScale * a_position;
+  pos = entityRotationMatrix * (pos - entityOrigin) + entityOrigin + entityTranslation;
+  gl_Position = viewportScale * (viewportTranslation + vec4(pos, uEntityZ, 1) );
+}
+*/
+
+
+var VERTEX_SHADER_SRC = 
+"\r\nattribute vec2 a_position;\r\nuniform float uViewportWidth;\r\nuniform float uViewportHeight;\r\nuniform float uViewportX;\r\nuniform float uViewportY;\r\n\r\nuniform float uEntityWidth;\r\nuniform float uEntityHeight;\r\nuniform float uEntityX;\r\nuniform float uEntityY;\r\nuniform float uEntityZ;\r\n\r\nuniform float uOriginX;\r\nuniform float uOriginY;\r\nuniform float uEntityRotation;\r\n\r\n\r\nmat4 viewportScale = mat4(2.0 \/ uViewportWidth,0,0,0, 0,-2.0 \/ uViewportHeight, 0,0, 0,0,1,0, -1, +1.0,0,1);\r\nvec4 viewportTranslation = vec4(uViewportX, uViewportY, 0, 0);\r\n\r\nmat2 entityScale = mat2( uEntityWidth, 0, 0, uEntityHeight);\r\nvec2 entityTranslation = vec2(uEntityX, uEntityY);\r\nvec2 entityOrigin = vec2(uOriginX, uOriginY);\r\nmat2 entityRotationMatrix = mat2(cos(uEntityRotation), sin(uEntityRotation), -sin(uEntityRotation), cos(uEntityRotation));\r\n\r\nvoid main() {\r\n  vec2 pos = entityScale * a_position;\r\n  pos = entityRotationMatrix * (pos - entityOrigin) + entityOrigin + entityTranslation;\r\n  gl_Position = viewportScale * (viewportTranslation + vec4(pos, uEntityZ, 1) );\r\n}"
+
+
+
+Crafty.c("TestSquare", {
+  init: function(){
+      if (this.has("WebGL")){
+        this._establishShader("TestSquare", this._fragmentShader)
+      }
+
+  },
+
+  _fragmentShader: FRAGMENT_SHADER_SRC
+
+});
+
+Crafty.c("TestSquareWhite", {
+  init: function(){
+      if (this.has("WebGL")){
+        this._establishShader("TestSquareWhite", this._fragmentShader)
+      }
+
+  },
+
+  _fragmentShader: FRAGMENT_SHADER_SRC_2
+
+});
+
+Crafty.c("TestColor", {
+  init: function(){
+      if (this.has("WebGL")){
+        this._establishShader("TestColor", this._fragmentShader)
+      }
+
+      this._red = this._blue = this._green = 1.0;
+      this.bind("Draw", this._drawColor)
+  },
+
+  _fragmentShader: 
+    "precision mediump float;"
+    + "uniform float uRed;"
+    + "uniform float uBlue;"
+    + "uniform float uGreen;"
+    + "void main(void) {"
+    + "  gl_FragColor = vec4(uRed, uGreen, uBlue, 1.0);"
+    + "}",
+
+  _drawColor: function(drawVars){
+    var gl = drawVars.gl, shaderProgram = drawVars.program;
+    var r = gl.getUniformLocation(shaderProgram, "uRed");
+    var g = gl.getUniformLocation(shaderProgram, "uGreen");
+    var b = gl.getUniformLocation(shaderProgram, "uBlue");
+    gl.uniform1f(r, this._red);
+    gl.uniform1f(g, this._green);
+    gl.uniform1f(b, this._blue);
+
+
+  },
+
+  color: function (r, g, b){
+    this._red = r;
+    this._green = g;
+    this._blue = b;
+  }
+
+});
 
 
 
 // This will totally assume, for now, that gl-matrix is available
 Crafty.c("WebGL", {
-
     init: function () {
         if (!Crafty.webgl.context) {
             Crafty.webgl.init();
         }
 
-        gl = Crafty.webgl.context;
+        var gl = Crafty.webgl.context;
 
         //increment the amount of canvas objs
         Crafty.webgl.entities++;
@@ -40,7 +145,7 @@ Crafty.c("WebGL", {
         Crafty.webgl.add(this);
 
 
-        this._vertexBuffer = gl.createBuffer();
+        this._vertexBuffer = Crafty.webgl.defaultVertexBuffer;
 
 
         this.bind("Change", function (e) {
@@ -99,6 +204,7 @@ Crafty.c("WebGL", {
     draw: function (ctx, x, y, w, h) {
 
         if (!this.ready) return;
+
         if (arguments.length === 4) {
             h = w;
             w = y;
@@ -119,16 +225,9 @@ Crafty.c("WebGL", {
 
         // FIXME defaults to z=0
         // Shouldn't actually be calculated every time!
-        this._calculateVertices()
+        //this._calculateVertices()
 
 
-        //console.log("Vertices calculated")
-
-
-        // Set rotation around origin
-        if (this._rotation !== 0) {
-            
-        }
 
         // Handle flipX, flipY
         if (this._flipX || this._flipY) {
@@ -142,13 +241,11 @@ Crafty.c("WebGL", {
         }
 
         //Draw entity
+        var gl = Crafty.webgl.context;
+        this.drawVars.gl = gl;
+        var shaderProgram = this._shaderProgram;
+        this.drawVars.program = shaderProgram;
 
-        //Just draw a white square for now!
-
-        //Set the mvMatrix to just be the identity!  No viewport translations yet.
-        // TODO switch to explicitly 2D representation?
- 
-        var shaderProgram = Crafty.webgl._shaderProgram;
         gl.bindBuffer(gl.ARRAY_BUFFER, this._vertexBuffer);
         gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this._vertexBuffer.itemSize, gl.FLOAT, false, 0, 0) 
 
@@ -161,10 +258,12 @@ Crafty.c("WebGL", {
         var h = gl.getUniformLocation(shaderProgram, "uEntityHeight");
         var x = gl.getUniformLocation(shaderProgram, "uEntityX");
         var y = gl.getUniformLocation(shaderProgram, "uEntityY");
+        var z = gl.getUniformLocation(shaderProgram, "uEntityZ");
         gl.uniform1f(w, pos._w);
         gl.uniform1f(h, pos._h);
         gl.uniform1f(x, pos._x);
         gl.uniform1f(y, pos._y);
+        gl.uniform1f(z, 1/this._globalZ);
 
         x = gl.getUniformLocation(shaderProgram, "uOriginX");
         y = gl.getUniformLocation(shaderProgram, "uOriginY");
@@ -175,13 +274,9 @@ Crafty.c("WebGL", {
         gl.uniform1f(rot, this._rotation);
 
 
-        //console.log("About to draw arrays")
+        this.trigger("Draw", this.drawVars);
+
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, this._vertexBuffer.numItems);
-
-        //this.drawVars.ctx = context;
-        //this.trigger("Draw", this.drawVars);
-
-        //console.log("Draw complete?")
         
         return this;
     },
@@ -207,7 +302,19 @@ Crafty.c("WebGL", {
     	// Create a matrix representing the transformations necessary before rendering this entity
     	
 
-    }
+    },
+
+    // v_src is optional, there's a default vertex shader that works for regular rectangular entities
+    _establishShader: function(compName, f_src, v_src){
+        var wgl = Crafty.webgl;
+        if (typeof wgl.programs[compName] === "undefined"){
+          wgl.programs[compName] = wgl.makeProgram(f_src, v_src);
+        }
+          
+        this._shaderProgram = wgl.programs[compName];
+
+        this.ready = true;
+    },
 });
 
 /**@
@@ -217,6 +324,7 @@ Crafty.c("WebGL", {
  * Collection of methods to draw on canvas.
  */
 Crafty.extend({
+
     webgl: {
         /**@
          * #Crafty.webgl.context
@@ -237,6 +345,36 @@ Crafty.extend({
          *
          * WebGL Canvas element
          */
+
+        programs: {},
+        compileShader: function (src, type){
+            var gl = this.context;
+            var shader = gl.createShader(type);
+            gl.shaderSource(shader, src);
+            gl.compileShader(shader);
+            if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+              throw(gl.getShaderInfoLog(shader));
+            };
+            return shader;
+
+        },
+
+        makeProgram: function (fragment_src, vertex_src){
+            var gl = this.context;
+            var fragment_shader = this.compileShader(fragment_src, gl.FRAGMENT_SHADER);
+            var vertex_shader = (vertex_src) ? this.compileShader(vertex_src, gl.VERTEX_SHADER) : this.defaultVertexShader;
+
+            var shaderProgram = gl.createProgram();
+            gl.attachShader(shaderProgram, vertex_shader);
+            gl.attachShader(shaderProgram, fragment_shader);
+            gl.linkProgram(shaderProgram);
+
+            if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+              throw("Could not initialise shaders");
+            }
+            return shaderProgram;
+        },
+
 
         /**@
          * #Crafty.webgl.init
@@ -275,17 +413,18 @@ Crafty.extend({
 
             // Equivalent of initGL in sample prog
             var gl;
-		    try {
-		      gl = c.getContext("webgl") || c.getContext("experimental-webgl");
-		      gl.viewportWidth = c.width;
-		      gl.viewportHeight = c.height;
-		    } catch(e) {
-                //Do nothing!
-		    }
+    		    try {
+    		      gl = c.getContext("webgl") || c.getContext("experimental-webgl");
+    		      gl.viewportWidth = c.width;
+    		      gl.viewportHeight = c.height;
+    		    } catch(e) {
+                    //Do nothing!
+    		    }
 
-		    if (!gl) {
-		      Crafty.trigger("NoWebGL");
-		    }
+    		    if (!gl) {
+    		      Crafty.trigger("NoWebGL");
+              return;
+    		    }
             Crafty.webgl.context = gl;
             Crafty.webgl._canvas = c;
 
@@ -301,41 +440,31 @@ Crafty.extend({
             // Equivalent of initShaders in sample prog
             //
 
-            var fragment_shader = gl.createShader(gl.FRAGMENT_SHADER);
-            gl.shaderSource(fragment_shader, FRAGMENT_SHADER_SRC);
-            gl.compileShader(fragment_shader);
-            if (!gl.getShaderParameter(fragment_shader, gl.COMPILE_STATUS)) {
-              alert(gl.getShaderInfoLog(fragment_shader));
-              return;
-            };
-            this._fragment_shader = fragment_shader;
+            
+            this.defaultVertexShader = this.compileShader(VERTEX_SHADER_SRC, gl.VERTEX_SHADER);
+            
 
-            var vertex_shader = gl.createShader(gl.VERTEX_SHADER);
-            gl.shaderSource(vertex_shader, VERTEX_SHADER_SRC);
-            gl.compileShader(vertex_shader);
-            if (!gl.getShaderParameter(vertex_shader, gl.COMPILE_STATUS)) {
-              alert(gl.getShaderInfoLog(vertex_shader));
-            };
-            this._vertex_shader = vertex_shader;
-
-            var shaderProgram = gl.createProgram();
-            gl.attachShader(shaderProgram, vertex_shader);
-            gl.attachShader(shaderProgram, fragment_shader);
-            gl.linkProgram(shaderProgram);
-
-            if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-              alert("Could not initialise shaders");
-            }
+            var shaderProgram = this.makeProgram(FRAGMENT_SHADER_SRC);
             this._shaderProgram = shaderProgram;
-
-
 
             gl.useProgram(shaderProgram);
 
+            
+            this.defaultVertexBuffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.defaultVertexBuffer)
+            var vertices = [
+              0, 0,
+              0, 1,
+              1, 0,
+              1, 1
+            ];
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+            this.defaultVertexBuffer.itemSize = 2;
+            this.defaultVertexBuffer.numItems = 4;
+
+
             shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "a_position");
             gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-            //shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
-            //shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
 
             //
             // End temp program!
@@ -349,6 +478,19 @@ Crafty.extend({
 
             //Bind rendering of canvas context (see drawing.js)
             Crafty.uniqueBind("RenderScene", Crafty.webgl.render);
+
+        },
+
+        setViewportUniforms: function(shaderProgram){
+            gl = Crafty.webgl.context;
+            var w = gl.getUniformLocation(shaderProgram, "uViewportWidth");
+            var h = gl.getUniformLocation(shaderProgram, "uViewportHeight");
+            var x = gl.getUniformLocation(shaderProgram, "uViewportX");
+            var y = gl.getUniformLocation(shaderProgram, "uViewportY");
+            gl.uniform1f(w, Crafty.viewport.width);
+            gl.uniform1f(h, Crafty.viewport.height);
+            gl.uniform1f(x, Crafty.viewport._x);
+            gl.uniform1f(y, Crafty.viewport._y);
 
         },
 
@@ -369,21 +511,18 @@ Crafty.extend({
 
             //Set the viewport uniform variables
             var shaderProgram = Crafty.webgl._shaderProgram;
-            var w = gl.getUniformLocation(shaderProgram, "uViewportWidth");
-            var h = gl.getUniformLocation(shaderProgram, "uViewportHeight");
-            var x = gl.getUniformLocation(shaderProgram, "uViewportX");
-            var y = gl.getUniformLocation(shaderProgram, "uViewportY");
-            gl.uniform1f(w, Crafty.viewport.width);
-            gl.uniform1f(h, Crafty.viewport.height);
-            gl.uniform1f(x, Crafty.viewport._x);
-            gl.uniform1f(y, Crafty.viewport._y);
-
+            
+            
 
 
             for (; i < l; i++) {
                 current = q[i];
                 if (current._visible && current.__c.WebGL) {
                     //console.log("rendering a thing #" + current[0])
+                    
+                    shaderProgram = current._shaderProgram;
+                    gl.useProgram(shaderProgram);
+                    Crafty.webgl.setViewportUniforms(shaderProgram);
                     current.draw();
                     current._changed = false;
                 }
