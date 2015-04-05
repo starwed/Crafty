a4f163b4fa6414e14a5ff4c83e7c572b2cac6729
Tim Martin <starwed@gmail.com>
2015-04-05 13:46:19 -0400

    Testing the build script

--- travis-nightly.sh
+++ travis-nightly.sh
@@ -1,10 +1,10 @@
 # If this isn't a PR, and occurs on the develop branch, push crafty.js to craftyjs/Crafty-Distro
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "develop"]; then
-  echo -e "Uploading nightly build to craftyjs/Crafty-Distro\n" 
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "my-travis"]; then
+  echo -e "Uploading nightly build to Crafty-Distro\n" 
   # Log some git info to the file commit_date
   TZ=UTC git log -n 1 --format=format:"built from commit %h (%cd) of 'develop' branch" --date=local > commit_date
   #Clone the nightlies branch into the nightlies directory
-  git clone -b nightlies https://${GH_TOKEN}@github.com/craftyjs/Crafty-Distro.git $HOME/nightlies
+  git clone -b nightlies https://git@github.com/craftyjs-machine-user/Crafty-Distro.git $HOME/nightlies
   mv $TRAVIS_BUILD_DIR/crafty.js $HOME/nightlies
   mv $TRAVIS_BUILD_DIR/commit_date $HOME/nightlies/README.md
   cd $HOME/nightlies
