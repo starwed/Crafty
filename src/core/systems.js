303b8f9d58506abc381d6a40a3aea15090c75348
Tim Martin <starwed@gmail.com>
2016-04-17 16:26:21 -0400

    Add system options

--- src/core/systems.js
+++ src/core/systems.js
@@ -13,10 +13,11 @@ Crafty._systems = {};
  * @trigger SystemLoaded - When the system has initialized itself - obj - system object
  * @trigger SystemDestroyed - Right before the system is destroyed - obj - system object
  *
- * @sign void Crafty.s(String name, Obj template[, Boolean lazy])
+ * @sign void Crafty.s(String name, Obj template[, Obj options][, Boolean lazy])
  * Register a system
  * @param name - The name of the system
  * @param template - an object whose methods and properties will be copied to the new system
+ * @param options - an object whose properties will be deep copied to the new system's options property
  * @param lazy - a flag that indicates whether the system should be initialized right away or the first time it is referenced
  *
  * @sign System Crafty.s(String name)
@@ -29,122 +30,152 @@ Crafty._systems = {};
  * This new system will automatically have the following event related methods, which function like those of components: `.bind()`, `unbind()`, `trigger()`, `one()`, `uniqueBind()`, `destroy()`.
  * Much like components, you can also provide `init()` and `remove()` methods, as well as an `events` parameter for automatically binding to events.
  *
- * *Note*: The `init()` method is for setting up the internal state of the system -- if you create entities in it that then reference the system, that'll create an infinite loop.
+ * @note The `init()` method is for setting up the internal state of the system -- if you create entities in it that then reference the system, that'll create an infinite loop.
  */
-Crafty.s = function(name, obj, lazy) {
-	if (obj) {
-		if (lazy === false ) {
-			Crafty._systems[name] = new Crafty.CraftySystem(name, obj);
-			Crafty.trigger("SystemLoaded", name);
-		} else {
-			Crafty._registerLazySystem(name, obj);
-		}
-	} else {
-		return Crafty._systems[name];
-	}
+Crafty.s = function(name, obj, options, lazy) {
+    if (obj) {
+        if (typeof options === "boolean") {
+            lazy = options;
+            options = null;
+        }
+        if (lazy === false) {
+            Crafty._systems[name] = new Crafty.CraftySystem(name, obj, options);
+            Crafty.trigger("SystemLoaded", name);
+        } else {
+            Crafty._registerLazySystem(name, obj, options);
+        }
+    } else {
+        return Crafty._systems[name];
+    }
 };
 
 
+// merge() and optionMerge() are internal helper functions for now
+// If it turns out this would be useful elsewhere, they should be moved out to core?
+function merge(defaults, specific){
+    // Return defaults if specific is not defined
+    if (typeof specific === "undefined") return defaults;
+    // Return specified values if no further merging is required
+    if (typeof specific !== "object" || typeof defaults === "undefined") return specific;
+    // Merge default values into specific using recursion
+    for (var key in defaults){
+        specific[key] = merge(defaults[key], specific[key]);
+    }
+    return specific;
+}
 
-Crafty._registerLazySystem = function(name, obj) {
-	// This is a bit of magic to only init a system if it's requested at least once.
-	// We define a getter for _systems[name] that will first initialize the system, 
-	// and then redefine _systems[name] to remove that getter.
-	Object.defineProperty(Crafty._systems, name, {
-		get: function() {
-			Object.defineProperty(Crafty._systems, name, { 
-				value: new Crafty.CraftySystem(name, obj),
-				writable: true,
-				enumerable: true,
-				configurable: true
-			});
-			Crafty.trigger("SystemLoaded", name);
-			return Crafty._systems[name];
-		},
-		configurable: true
-	});
+function optionMerge(defaults, specific){
+    // Deep copy the objects, then merge them
+    // This merge treats `null` as a specified value
+    defaults = Crafty.clone(defaults);
+    specific = Crafty.clone(specific);
+    return merge(defaults, specific);
+}
+
+
+Crafty._registerLazySystem = function(name, obj, options) {
+    // This is a bit of magic to only init a system if it's requested at least once.
+    // We define a getter for _systems[name] that will first initialize the system, 
+    // and then redefine _systems[name] to remove that getter.
+    Object.defineProperty(Crafty._systems, name, {
+        get: function() {
+            Object.defineProperty(Crafty._systems, name, {
+                value: new Crafty.CraftySystem(name, obj, options),
+                writable: true,
+                enumerable: true,
+                configurable: true
+            });
+            Crafty.trigger("SystemLoaded", name);
+            return Crafty._systems[name];
+        },
+        configurable: true
+    });
 
 };
 
 // Each system has its properties and methods copied onto an object of this type
-Crafty.CraftySystem = (function(){
-	systemID = 1;
-	return function(name, template) {
-		this.name = name;
-		if (!template) return this;
-		this._systemTemplate = template;
-		this.extend(template);
+Crafty.CraftySystem = (function() {
+    systemID = 1;
+    return function(name, template, options) {
+        this.name = name;
+        if (!template) return this;
+        this._systemTemplate = template;
+        this.extend(template);
+        
+        // Overwrite any default options with the passed options object
+        // This does a deep copy on the objects, and treats null as a specified value
+        this.options = optionMerge(this.options, options);
 
-		// Add the "low leveL" callback methods
-		Crafty._addCallbackMethods(this);
+        // Add the "low leveL" callback methods
+        Crafty._addCallbackMethods(this);
 
-		// Give this object a global ID.  Used for event handlers.
-		this[0] = "system" + (systemID++);
-		// Run any instantiation code
-		if (typeof this.init === "function") {
-			this.init(name);
-		}
-		// If an events object is provided, bind the listed event handlers
-		if ("events" in template){
-			var auto = template.events;
-			for (var eventName in auto){
-				var fn = typeof auto[eventName] === "function" ? auto[eventName] : template[auto[eventName]];
-				this.bind(eventName, fn);
-			}
-		}
-	};
+        // Give this object a global ID.  Used for event handlers.
+        this[0] = "system" + (systemID++);
+        // Run any instantiation code
+        if (typeof this.init === "function") {
+            this.init(name);
+        }
+        // If an events object is provided, bind the listed event handlers
+        if ("events" in template) {
+            var auto = template.events;
+            for (var eventName in auto) {
+                var fn = typeof auto[eventName] === "function" ? auto[eventName] : template[auto[eventName]];
+                this.bind(eventName, fn);
+            }
+        }
+    };
 })();
 
 
 
 Crafty.CraftySystem.prototype = {
-	extend: function(obj) {
-		// Copy properties and methods of obj
-		for (var key in obj) {
-			if (typeof this[key] === "undefined") {
-				this[key] = obj[key];
-			}
-		}
-	},
+    extend: function(obj) {
+        // Copy properties and methods of obj
+        for (var key in obj) {
+            if (typeof this[key] === "undefined") {
+                this[key] = obj[key];
+            }
+        }
+    },
 
-	// Event methods
-	bind: function(event, callback) {
-		this._bindCallback(event, callback);
-		return this;
-	},
+    // Event methods
+    bind: function(event, callback) {
+        this._bindCallback(event, callback);
+        return this;
+    },
 
-	trigger: function(event, data) {
-		this._runCallbacks(event, data);
-		return this;
-	},
+    trigger: function(event, data) {
+        this._runCallbacks(event, data);
+        return this;
+    },
 
-	unbind: function(event, callback) {
-		this._unbindCallbacks(event, callback);
-		return this;
-	},
+    unbind: function(event, callback) {
+        this._unbindCallbacks(event, callback);
+        return this;
+    },
 
-	one: function (event, callback) {
-		var self = this;
-		var oneHandler = function (data) {
-			callback.call(self, data);
-			self.unbind(event, oneHandler);
-		};
-		return self.bind(event, oneHandler);
-	},
+    one: function(event, callback) {
+        var self = this;
+        var oneHandler = function(data) {
+            callback.call(self, data);
+            self.unbind(event, oneHandler);
+        };
+        return self.bind(event, oneHandler);
+    },
 
-	uniqueBind: function(event, callback) {
-		this.unbind(event, callback);
-		return this.bind(event, callback);
-	},
+    uniqueBind: function(event, callback) {
+        this.unbind(event, callback);
+        return this.bind(event, callback);
+    },
 
-	destroy: function() {
-		Crafty.trigger("SystemDestroyed", this);
-		// Check the template itself
-		if (typeof this.remove === "function") {
-			this.remove();
-		}
-		this._unbindAll();
-		delete Crafty._systems[this.name];
-	}
+    destroy: function() {
+        Crafty.trigger("SystemDestroyed", this);
+        // Check the template itself
+        if (typeof this.remove === "function") {
+            this.remove();
+        }
+        this._unbindAll();
+        delete Crafty._systems[this.name];
+    }
 
 };
